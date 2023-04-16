fad2bfde520c1191215d5ca458837f2630e1e011
Eunseo <supreme327@naver.com>
2023-04-16 21:08:17 +0900

    04.16

--- review.js
+++ review.js
@@ -3,20 +3,25 @@
 function onClickLike() {
     const likeNum = document.getElementById('like-num');
     let number = likeNum.innerText;
-    // 이미지 변경을 위한 likeBtn 변수 선언
     const likeBtn = document.getElementById('like-btn');
 
     if (likeBtn.classList.contains('active')) {
-        // 이미 클릭되어 활성화된 경우, 함수 종료
+        // 이미 클릭되어 활성화된 경우, active 클래스 제거하고 likeNum 감소
+        likeBtn.classList.remove('active');
+        likeNum.innerText = parseInt(number) - 1;
+        // 이미지 변경
+        likeBtn.src = 'img/like.png';
         return;
     }
     // likeNum 증가
     number = parseInt(number) + 1;
     // likeNum에 결과값 출력
     likeNum.innerText = number;
-    // 이미지 변경 코드
+    // 이미지 변경
     likeBtn.src = 'img/like-on.png';
     likeBtn.classList.add('active');
 }
+
+
 
 
